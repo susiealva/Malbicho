0bc8823537e6da4a6f4c63562671d767999ce284
AngelVillaDev <villalopezluis99@gmail.com>
2021-05-10 03:04:07 -0700

    routes for all the pages and case conflicts solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CerveceriaComponent } from './components/cerveceria/cerveceria.component';
+import { CervezasComponent } from './components/cervezas/cervezas.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { PromocionesComponent } from './components/promociones/promociones.component';
+import { TastingRoomComponent } from './components/tasting-room/tasting-room.component';
 
 const routes: Routes = [
 {path: '', component: HomePageComponent},
+{path: 'cerveceria', component: CerveceriaComponent},
+{path: 'cervezas', component: CervezasComponent},
+{path: 'promociones', component: PromocionesComponent},
+{path: 'tasting-room', component: TastingRoomComponent},
 {path: 'landing', component: LandingPageComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { PromocionesComponent } from './Components/promociones/promociones.component';
-import { CerveceriaComponent } from './Components/cerveceria/cerveceria.component';
-import { TastingRoomComponent } from './Components/tasting-room/tasting-room.component';
-import { CervezasComponent } from './Components/cervezas/cervezas.component';
+import { PromocionesComponent } from './components/promociones/promociones.component';
+import { CerveceriaComponent } from './components/cerveceria/cerveceria.component';
+import { TastingRoomComponent } from './components/tasting-room/tasting-room.component';
+import { CervezasComponent } from './components/cervezas/cervezas.component';
 
 @NgModule({
   declarations: [
