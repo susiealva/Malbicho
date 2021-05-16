7353e146fc2fe28f5cea7b12be4119c0fba2115c
AngelVillaDev <villalopezluis99@gmail.com>
2021-05-16 02:08:08 -0700

    Fixed Component erro, fixed CSS, refactoring

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,17 +1,10 @@
 import { DeclarationListEmitMode } from '@angular/compiler';
 import { Component, NgModule, OnInit } from '@angular/core';
-import { ModuloPromocionComponent } from '../subComponents/modulo-promocion.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
-})
-
-@NgModule({
-  declarations: [
-    ModuloPromocionComponent
-  ]
 })
 
 export class HomePageComponent implements OnInit {
