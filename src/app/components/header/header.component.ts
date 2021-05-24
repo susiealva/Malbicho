eaf38e0c3b542dbf56e0af9523d6f5dfe02860b7
AngelVillaDev <villalopezluis99@gmail.com>
2021-05-24 14:16:28 -0700

    Contact update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,8 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+ 
+  
+  
   ngOnInit(): void {
+  
   }
 
 }
